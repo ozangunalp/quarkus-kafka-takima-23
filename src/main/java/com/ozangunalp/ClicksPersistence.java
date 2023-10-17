f1a9a5eeb8a86de04a0189cdf1968ffb1800857f
Ozan Gunalp <ozangunalp@gmail.com>
2023-10-17 18:44:49 +0200

    Step 2. Persist clicks with batch consume

--- src/main/java/com/ozangunalp/ClicksPersistence.java
+++ src/main/java/com/ozangunalp/ClicksPersistence.java
@@ -3,12 +3,22 @@ package com.ozangunalp;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import io.quarkus.logging.Log;
 
 @ApplicationScoped
 public class ClicksPersistence {
 
+    @Incoming("clicks")
+    @Transactional
     void persistClicks(List<PointerEvent> clicks) {
-        // TODO 2 persist records consumed from clicks topic
+        for (PointerEvent click : clicks) {
+            PointerEventDTO.toDto(click).persist();
+        }
+        Log.infof("Persisted clicks %s", clicks);
     }
 
 }
--- src/main/java/com/ozangunalp/PointerEventDTO.java
+++ src/main/java/com/ozangunalp/PointerEventDTO.java
@@ -1,6 +1,10 @@
 package com.ozangunalp;
 
-public class PointerEventDTO {
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity(collection = "PointerEvent")
+public class PointerEventDTO extends PanacheMongoEntity {
 
     public String userId;
     public String sessionId;
