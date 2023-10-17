9855f48074f431b5e1527e6f4ecaa0b30a13fb5b
Ozan Gunalp <ozangunalp@gmail.com>
2023-10-17 16:42:02 +0200

    Step 1. Send clicks

--- src/main/java/com/ozangunalp/ClicksResource.java
+++ src/main/java/com/ozangunalp/ClicksResource.java
@@ -5,13 +5,23 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import io.quarkus.logging.Log;
+
 @Path("/clicks")
 public class ClicksResource {
+
+    @Channel("clicks-out")
+    Emitter<PointerEvent> clicks;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public void postClick(PointerEvent click) {
         // TODO 1 Send click to clicks topic
+        clicks.send(click);
+        Log.infof("Click sent %s", click);
     }
 
 }
